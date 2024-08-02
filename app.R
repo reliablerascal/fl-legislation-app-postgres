b2a0fe7636b5f850ed00b3be2e2210dbf9cbf03d
apantazi <andrew.pantazi@gmail.com>
2024-08-02 04:53:04 -0400

    added descriptions of libraries.
    duplicated app03 data to help protect modularization.
    
    added a fourth server to capture legislators' voting histories, ran this server inside the third server's UI.

--- app.R
+++ app.R
@@ -18,9 +18,10 @@ library(plotly)
 library(ggplot2)
 library(patchwork) # combines multiple ggplot2 plots into a single cohesive layout. Used for demographics bar charts
 
-library(DBI)
-library(RPostgres)
-library(scales)
+library(DBI) # access fl_leg_votes database
+library(RPostgres) # access fl_leg_votes database
+library(scales) # format as percent
+library(config) # for securely tracking API keys on shinyapps.io
 
 library(shinydisconnect) #customize Shiny app disconnect message
 
@@ -41,6 +42,8 @@ app02_leg_activity <- all_data$app02_leg_activity
 jct_bill_categories <- all_data$jct_bill_categories
 app03_district_context <- all_data$app03_district_context
 app03_district_context_state <- all_data$app03_district_context_state
+app04_district_context <- all_data$app04_district_context
+source("servers/voting_history_module.R", local = TRUE)
 
 ########################
 #                      #  
@@ -59,13 +62,35 @@ source("ui.R", TRUE)
 
 #local = TRUE ensures each sourced file has access to input/output/session
 server <- function(input, output, session) {
-  # log_message <- function(message) {
-  #   cat(message, "\n", file = "debug_log.txt", append = TRUE)
-  # }
+  # observe({
+  #   print(input$app_url)
+  #   # Manually set input$app_url for local testing
+  #   if (is.null(input$app_url)) {
+  #     updateTextInput(session, "app_url", value = "fl-leg-staging")
+  #   }
+  # })
+  # # display banner to identify staging app
+  # output$stagingBanner <- renderUI({
+  #   if (!is.null(input$app_url) && grepl("fl-leg-staging", input$app_url)) {
+  #     div(class = "staging-banner", "Staging App")
+  #   }
+  # })
+  # log_message <- function(message) {cat(message, "\n", file = "debug_log.txt", append = TRUE)}
   source("servers/server1_vote_patterns.R", local = TRUE)
-  # source("servers/server2_leg_activity.R", local = TRUE)
+  #source("servers/server2_leg_activity.R", local = TRUE)
   source("servers/server3_district_context.R", local = TRUE)
   source("servers/server4_partisanship_scatterplot.R", local = TRUE)
+  source("servers/server5_legislator_lookup.R", local = TRUE)
+  #print(paste("Number of rows in app02_leg_activity:", nrow(app02_leg_activity)))  # Debug print
+  #print(head(app02_leg_activity))  # Debug print
+  output$debug_output <- renderPrint({
+    print("Columns in app02_leg_activity:")
+    print(names(app02_leg_activity))
+    print("Summary of vote_with_neither:")
+    print(summary(app02_leg_activity$vote_with_neither))
+    print("Summary of maverick_votes:")
+    print(summary(app02_leg_activity$maverick_votes))
+  })
 }
 
 ########################
