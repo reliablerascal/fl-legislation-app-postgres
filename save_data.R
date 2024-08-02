8401246d6c3a56b76d8c3e1965e37dc34fdfa2b5
apantazi <andrew.pantazi@gmail.com>
2024-08-02 04:54:25 -0400

    uncomment legislative history data. create data subdirectory if subdirectory doesn't already exist

--- save_data.R
+++ save_data.R
@@ -1,9 +1,14 @@
+if (!dir.exists("data")) {
+  dir.create("data")
+}
+
 all_data <- list(
   app01_vote_patterns = app01_vote_patterns,
-  #app02_leg_activity = app02_leg_activity,
+  app02_leg_activity = app02_leg_activity,
   jct_bill_categories = jct_bill_categories,
   app03_district_context = app03_district_context,
-  app03_district_context_state = app03_district_context_state
+  app03_district_context_state = app03_district_context_state,
+  app04_district_context = app04_district_context
 )
 
 saveRDS(all_data, file = "data/all_data.rds")
